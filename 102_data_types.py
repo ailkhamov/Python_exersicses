da4e886329ecbc2feff396a4cd01b169b48d3c30
Abror Ilkhamov <AIlkhamov@spartaglobal.com>
2019-09-23 11:04:03 +0100

    Comparission operator added

---
+++ 102_data_types.py
@@ -0,0 +1,33 @@
+# Numerical Data types
+# - Ints, long, float, comples
+# THese are numerical data types which we can use numberical operators
+
+
+# Complex and long number we don't use as much
+# Complex bring an imaginary type of number
+# Long - re integers of unlimited size
+#
+# int = Stands for integers
+# Whole numbers
+my_int  = 10
+print(my_int)
+print(type(my_int))
+
+print(4/2) # Float
+print(4//2) # Integer
+
+# Comparission Operator
+# == - Comparison Operator
+#  </ > - Bigger and small than
+# <= -Small than or equal
+#  >= - bigger than or equel
+#  != - Not equal
+#  is and is not
+
+variable1 = 10
+variable2 = 13
+print(variable1 == variable2)
+print(variable1 > variable2)
+print(variable1< variable2)
+
+#Boolean Values
