a26e0ed95425bc1ae0174d6e35d8b468d7032f9f
Abror Ilkhamov <AIlkhamov@spartaglobal.com>
2019-09-23 17:55:44 +0100

    Home work completed

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -2,11 +2,33 @@
 # Use variables and different data types
 # Ask and store the following in variables:
 # name, last_name. age, age_of_mother, 1 skills
+
+name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+age_of_mother = int(input("Enter the age of your mother: "))
+skill = input("Enter your three skills: ")
+
 # Print out the information in a formatted manner
+print(f'Your name is {name.lower().capitalize()} {last_name.lower().capitalize()}. Your age is {age}! Your mothers age is {age_of_mother}, your skills are {skill}!')
+
 # Calculate age difference between response and mother
+age_difference = age_of_mother - age
+if age_of_mother < age:
+    print('You cannot be older than your mother! Please enter the correct age')
+else:
+    print(f'The age diffence between you and your mother is {age_difference}')
 
 # Store skills in a list.
+
+# Convert string to a list
+def Convert_to_string(string):
+    li = list(string.split(" "))
+    return li
+
+print(Convert_to_string(skill))
+
 # Print each skill in a formatted way (a little context text), appropriate string formatting
 # like lower case or upper case etc
+print(skill.upper())
 
-#
--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -2,10 +2,26 @@
 #
 # # Define an empty dictionary
 # empty = {}
-#
+
+hero = input("Enter your hero: ")
+start = input("Enter the start of the story: ")
+middle = input("Enter the middle of your story: ")
+end = input("Enter the end of your story: ")
+
+dictionary = {
+    'Hero': hero,
+    'Start': start,
+    'Middle': middle,
+    'End': end
+}
+
+# Print out the dictionary information in an ordered way so we can scan read the story.
+for key, values in dictionary.items():
+    print(key,values)
+
 # Prompt user for input for a story
 #     it should have: hero, beginning, middle, end
 #      2 other things you define to be part of the story
 #     add each response to the dictionary under an appropriate key
 #
-#     Print out the dictionary information in an ordered way so we can scan read the story.
+
