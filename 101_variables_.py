dceb88e1c9f5d4a8a02a1d6f7d7d2e8e0e815496
Abror Ilkhamov <AIlkhamov@spartaglobal.com>
2019-09-23 10:39:15 +0100

    Variables covered

---
+++ 101_variables_.py
@@ -0,0 +1,19 @@
+# Variables in python
+# Variable is a box. You can give it a name and put stuff inside
+
+box_variable = 'Books and stuff'
+
+print(box_variable)
+
+# Variables are mutable - Hence they can change/be re-assigned
+box_variable = 'CDs  and other stuff'
+print(box_variable)
+
+# Important python commands
+# print(arguments)
+print('Hello') # printing string
+print(box_variable) # printing variable
+print('Hello', box_variable) # overloading with two arguments
+
+# print(type(arg)
+print(type("hello"))
